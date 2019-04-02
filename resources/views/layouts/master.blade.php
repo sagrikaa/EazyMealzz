7ebc2b1a95e0da79eb1f11c46293b9f588e3c364
Singh, Harshdeep <singh.harshdeep@live.in>
2019-04-02 15:53:51 -0400

    Changes login route

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,7 +40,7 @@
                     <a class="nav-link" href="#">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
                 </li>
                 
                 </li>
