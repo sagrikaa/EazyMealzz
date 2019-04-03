a0be80852cde053f9dd386699be9e5013ee4e741
Singh, Harshdeep <singh.harshdeep@live.in>
2019-04-03 11:49:47 -0400

    Adds SQL dump

---
+++ easymealz.sql
@@ -0,0 +1,270 @@
+-- MySQL dump 10.13  Distrib 8.0.14, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: EasyMealz
+-- ------------------------------------------------------
+-- Server version	8.0.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `comment_string` varchar(45) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredients`
+--
+
+DROP TABLE IF EXISTS `ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `ingredients` (
+  `ingredient_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ingredient_name` varchar(45) NOT NULL,
+  `ingredient_description` varchar(45) DEFAULT NULL,
+  `serving_size` varchar(45) DEFAULT NULL,
+  `calories` decimal(5,2) DEFAULT NULL,
+  `fats` decimal(5,2) DEFAULT NULL,
+  `carbohydrates` decimal(5,2) DEFAULT NULL,
+  `proteins` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`ingredient_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredients`
+--
+
+LOCK TABLES `ingredients` WRITE;
+/*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
+INSERT INTO `ingredients` VALUES (1,'Egg',NULL,'1 Large',78.00,5.00,0.60,6.00),(2,'Milk',NULL,'100 grams',42.00,1.00,5.00,3.40),(3,'Bread',NULL,'1 slice',60.00,1.00,12.00,3.00),(4,'Egg',NULL,'1 Large',78.00,5.00,0.60,6.00),(5,'Milk',NULL,'100 grams',42.00,1.00,5.00,3.40),(6,'Bread',NULL,'1 slice',60.00,1.00,12.00,3.00),(7,'Egg',NULL,'1 Large',78.00,5.00,0.60,6.00),(8,'Milk',NULL,'100 grams',42.00,1.00,5.00,3.40),(9,'Bread',NULL,'1 slice',60.00,1.00,12.00,3.00),(10,'Egg',NULL,'1 Large',78.00,5.00,0.60,6.00),(11,'Milk',NULL,'100 grams',42.00,1.00,5.00,3.40),(12,'Bread',NULL,'1 slice',60.00,1.00,12.00,3.00),(13,'Egg',NULL,'1 Large',78.00,5.00,0.60,6.00),(14,'Milk',NULL,'100 grams',42.00,1.00,5.00,3.40),(15,'Bread',NULL,'1 slice',60.00,1.00,12.00,3.00),(16,'Egg',NULL,'1 Large',78.00,5.00,0.60,6.00),(17,'Milk',NULL,'100 grams',42.00,1.00,5.00,3.40),(18,'Bread',NULL,'1 slice',60.00,1.00,12.00,3.00),(19,'Egg',NULL,'1 Large',78.00,5.00,0.60,6.00),(20,'Milk',NULL,'100 grams',42.00,1.00,5.00,3.40),(21,'Bread',NULL,'1 slice',60.00,1.00,12.00,3.00),(22,'Egg',NULL,'1 Large',78.00,5.00,0.60,6.00),(23,'Milk',NULL,'100 grams',42.00,1.00,5.00,3.40),(24,'Bread',NULL,'1 slice',60.00,1.00,12.00,3.00);
+/*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_status` varchar(1000) DEFAULT NULL,
+  `post_link` varchar(45) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'Check out this amzing french toast recipe','http://www.easymealz.com/recipes/1',1);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipe_ingredients`
+--
+
+DROP TABLE IF EXISTS `recipe_ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `recipe_ingredients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recipe_id` int(11) DEFAULT NULL,
+  `ingredient_id` int(11) DEFAULT NULL,
+  `serving_size` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `recipe_id_idx` (`recipe_id`),
+  KEY `ingredient_id_idx` (`ingredient_id`),
+  CONSTRAINT `ingredient_id` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`),
+  CONSTRAINT `recipe_id` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipe_ingredients`
+--
+
+LOCK TABLES `recipe_ingredients` WRITE;
+/*!40000 ALTER TABLE `recipe_ingredients` DISABLE KEYS */;
+INSERT INTO `recipe_ingredients` VALUES (1,1,1,'1'),(2,1,2,'1/4 cup'),(3,1,3,'4 slices');
+/*!40000 ALTER TABLE `recipe_ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipes`
+--
+
+DROP TABLE IF EXISTS `recipes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `recipes` (
+  `recipe_id` int(11) NOT NULL AUTO_INCREMENT,
+  `recipe_name` varchar(45) NOT NULL,
+  `recipe_description` varchar(250) DEFAULT NULL,
+  `recipe_image_url` varchar(45) DEFAULT NULL,
+  `recipe_servings` varchar(45) NOT NULL,
+  `recipe_calories` varchar(45) DEFAULT NULL,
+  `recipe_steps` varchar(1000) NOT NULL,
+  PRIMARY KEY (`recipe_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipes`
+--
+
+LOCK TABLES `recipes` WRITE;
+/*!40000 ALTER TABLE `recipes` DISABLE KEYS */;
+INSERT INTO `recipes` VALUES (1,'French Toast','Vanilla extract and cinnamon bring a richness of flavor to French toast. Pick your favorite bread - white, Italian, French or whole wheat. Serve with Easy Spiced Syrup.',NULL,'4 servings','95','Beat egg, vanilla and cinnamon in shallow dish. Stir in milk. Dip bread in egg mixture, turning to coat both sides evenly. Cook bread slices on lightly greased nonstick griddle or skillet on medium heat until browned on both sides. Serve with Easy Spiced Syrup (recipe follows), if desired. Easy Spiced Syrup: Add 1 teaspoon McCormick® Pure Vanilla Extract and 1/4 teaspoon McCormick® Ground Cinnamon to 1 cup pancake syrup; stir well to mix. Serve warm, if desired.');
+/*!40000 ALTER TABLE `recipes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_groups`
+--
+
+DROP TABLE IF EXISTS `user_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_groups` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(45) DEFAULT NULL,
+  `group_id` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_groups`
+--
+
+LOCK TABLES `user_groups` WRITE;
+/*!40000 ALTER TABLE `user_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `type` varchar(1) NOT NULL DEFAULT 'u',
+  `avatar` varchar(45) DEFAULT NULL,
+  `remember_token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'testuser@gmail.com','testpass','User','u',NULL,NULL),(2,'testadmin@gmail.com','testpass','Admin','a',NULL,NULL),(17,'singh.harshdeep@live.in','$2y$10$DSCIdLNA7gSD80hrxB8eHe7YRYxB5lGxiqf1o7ExvdFcJqvjGphGO','Harshdeep Singh','u',NULL,NULL),(18,'test@gmail.com','$2y$10$QkRUGH.f4639OE5yVGMAYO5F/Ih5afyr91I3OFXNTXxwIQ2BSRYt2','Test','u',NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-03 11:48:19
