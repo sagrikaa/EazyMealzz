bb8a88fc9398768e06365ffe92f2f3aebeffd72d
Singh, Harshdeep <singh.harshdeep@live.in>
2019-04-07 16:07:15 -0400

    Improves Authentication Form UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,39 @@
 @extends('layouts.master')
 @section('content')
-
+<header>
+      <!--Carousel SLides-->
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item active" style="background-image: url({{asset('img/slider-1.jpg')}}">
+                    
+                </div>
+                <!-- Slide Two - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url({{asset('img/slider-2.jpg')}}">
+                    
+                </div>
+                <!-- Slide Three - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url({{asset('img/slider-3.jpg')}})">
+                    
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <!-- Search Bar -->
+        <div class="container search">
+            <div class="form-group">
+                <input type="text" class="form-control search-bar shadow" placeholder="Search Recipes">
+            </div>
+        </div>
+        
+    </header>
 <!-- Page Content -->
 <div class="container">
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,50 +37,19 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/contact">Contact</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">Login</a>
-                </li>
-                
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Login/Register
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                    <a class="dropdown-item" href="{{ route('register') }}">Register</a>
+                    </div>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
-
-    <header>
-      <!--Carousel SLides-->
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner" role="listbox">
-                <!-- Slide One - Set the background image for this slide in the line below -->
-                <div class="carousel-item active" style="background-image: url({{asset('img/slider-1.jpg')}}">
-                    
-                </div>
-                <!-- Slide Two - Set the background image for this slide in the line below -->
-                <div class="carousel-item" style="background-image: url({{asset('img/slider-2.jpg')}}">
-                    
-                </div>
-                <!-- Slide Three - Set the background image for this slide in the line below -->
-                <div class="carousel-item" style="background-image: url({{asset('img/slider-3.jpg')}})">
-                    
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-        <!-- Search Bar -->
-        <div class="container search">
-            <div class="form-group">
-                <input type="text" class="form-control search-bar shadow" placeholder="Search Recipes">
-            </div>
-        </div>
-        
-    </header>
     
     @yield('content')
     
