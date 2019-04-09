f4231bf6921779824e19b87dcf2b77ab848f9a43
sagrikaa <40899302+sagrikaa@users.noreply.github.com>
2019-04-09 18:32:52 -0400

    SQL DUMP

--- easymealz.sql
+++ easymealz.sql
@@ -16,6 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_title` varchar(100) DEFAULT NULL,
+  `article_content` varchar(1000) DEFAULT NULL,
+  `article_image_url` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `comments`
 --
 
@@ -127,13 +152,17 @@ DROP TABLE IF EXISTS `posts`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `post_status` varchar(1000) DEFAULT NULL,
-  `post_link` varchar(45) NOT NULL,
+  `title` varchar(1000) DEFAULT NULL,
+  `description` varchar(500) NOT NULL,
+  `ingredients` varchar(1000) DEFAULT NULL,
+  `content` varchar(5000) NOT NULL,
+  `instructions` varchar(5000) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
+  `created_at` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -142,6 +171,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'EASY 20 MINUTE BUTTER CHICKEN','Quick 20-minute butter chicken is creamy and packed full of flavor. It’s simplified and cooked all in one pan and goes great with a side of rice or naan! ','1 tablespoon oil','Ever since moving to Alexandria Virginia we’ve been in luck with all the Indian, Pakistani, and Aghani restaurants around us. We love the cuisine because it’s loaded with flavor and spice and is great for cold nights. One dish we keep going back for is the butter chicken. The spicy creaminess is unforgettable and always makes us go back for more!','Heat a large skillet or medium saucepan over medium-high heat. Add the oil, butter, and onions and cook onions down until lightly golden, about 3-4 minutes. Add ginger and garlic and let cook for 30 seconds, stirring so it doesn’t burn.',17,'2016-01-17'),(2,'CREAMY AVOCADO EGG SALAD','Spice up the usual egg salad with the addition of avocado. Avocado makes a delicious and nutritious addition to egg salad and thanks to its naturally creamy texture, you can enjoy egg salad without the adding mayo! ','1 medium avocado pitted and peeled','If you haven’t tried avocado egg salad then it’s time you enjoy egg salad in a whole new way. A simple nutrition rich ingredient such as avocado with the addition of lemon, salt, pepper and your favorite herbs works wonders and adds much-needed flavor and zest to the typical egg salad. Play around with the herbs and use parsley, dill or chives or use my personal favorite, cilantro, and enjoy a wholesome flavor-packed dish.',NULL,17,'2018-08-15'),(3,'ROASTED GARLIC CAULIFLOWER','Crispy cauliflower roasted with garlic and olive oil in under 30 minutes. This super simple and flavorful side dish that is low carb and keto friendly and makes a delicious side or snack! ','Olive, coconut, or oil of choice,Freshly minced garlic,Salt and pepper to taste,Parmesan cheese to top, optional,A large heavy duty sheet pan','Cauliflower is one of the most unrated veggies and overshadowed by its sibling broccoli but luckily, I’ve got you covered. Cauliflower is loaded with antioxidants, fiber and aids digestions. It’s the perfect low carb and meat replacement for weight loss and easy to add to your diet. From cauliflower pizza to cauliflower wings, cauliflower mac and cheese and much more!','Cut cauliflower florets into bitesize pieces,toss cauliflower with oil, garlic and salt, and pepper,spread in a single layer on a large baking sheet<br>bake until golden and crispy',14,'2018-11-03');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -238,13 +268,12 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `user_type` varchar(1) NOT NULL,
-  `user_status` varchar(10) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `user_type` varchar(1) NOT NULL DEFAULT 'u',
+  `user_status` varchar(10) NOT NULL DEFAULT 'active',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -253,7 +282,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'testuser@gmail.com','testpass','Test','User','u','Active'),(2,'testadmin@gmail.com','testpass','Test','Admin','a','Active'),(3,'testuser@gmail.com','testpass','Test','User','u','Active'),(4,'testadmin@gmail.com','testpass','Test','Admin','a','Active'),(5,'testuser@gmail.com','testpass','Test','User','u','Active'),(6,'testadmin@gmail.com','testpass','Test','Admin','a','Active'),(7,'testuser@gmail.com','testpass','Test','User','u','Active'),(8,'testadmin@gmail.com','testpass','Test','Admin','a','Active'),(9,'testuser@gmail.com','testpass','Test','User','u','Active'),(10,'testadmin@gmail.com','testpass','Test','Admin','a','Active'),(11,'testuser@gmail.com','testpass','Test','User','u','Active'),(12,'testadmin@gmail.com','testpass','Test','Admin','a','Active'),(13,'testuser@gmail.com','testpass','Test','User','u','Active'),(14,'testadmin@gmail.com','testpass','Test','Admin','a','Active'),(15,'testuser@gmail.com','testpass','Test','User','u','Active'),(16,'testadmin@gmail.com','testpass','Test','Admin','a','Active');
+INSERT INTO `users` VALUES (1,'nithaya@gmail.com','123456','Nithaya','u','Active'),(2,'admin@gmail.com','123456','HarshDeep','a','Active'),(3,'testuser@gmail.com','testpass','Test','u','Active'),(4,'sagrika@gmail.com','testpass','Sagrika','a','Active'),(5,'testuser@gmail.com','testpass','Test','u','Active'),(6,'shashank@gmail.com','testpass','Shashank','a','Active'),(7,'testuser@gmail.com','testpass','Test','u','Active'),(8,'ruchi@gmail.com','testpass','Ruchi','a','Active'),(9,'testuser@gmail.com','testpass','Test','u','Active'),(10,'simaran@gmail.com','testpass','Simran','a','Active'),(11,'testuser@gmail.com','testpass','Test','u','Active'),(12,'testadmin@gmail.com','testpass','Test','a','Active'),(13,'testuser@gmail.com','testpass','Test','u','Active'),(14,'testadmin@gmail.com','testpass','Test','a','Active'),(15,'testuser@gmail.com','testpass','Test','u','Active'),(16,'testadmin@gmail.com','testpass','Test','a','Active'),(17,'sagrika224@gmail.com','$2y$10$RJ34ULLK9Afaz0qgF1c5JusaE63xn8oayHtIdvxKhyk8JrGTyhqN6','sagrika aggarwal','u','active');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -266,4 +295,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-03 12:46:46
+-- Dump completed on 2019-04-09 18:32:14
