d565b4e8e7f2717ae153eb41225f8785c1e062d8
18ruchisingh <38141052+18ruchisingh@users.noreply.github.com>
2019-04-14 21:06:51 -0400

    User Display

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('content')
+<div><a class="mobilea" href="#"></a></div>
+<div id="contain">
+  <div class="sidebar">
+    <ul id="nav">
+      <li><a  href="admin_dashboard">Dashboard</a></li>
+      <li><a  href="user">Users</a></li>
+      <li><a href="#">Recipes</a></li>
+      <li><a href="#">Posts</a></li>
+      <li><a href="#">Subscriptions</a></li>
+    </ul>
+  </div>
+  <div class="content">
+      @yield('admincontent')
+    </div>
+@endsection
