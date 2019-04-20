340e6e5deb9dac56b22938f814f220396c8bbe41
18ruchisingh <38141052+18ruchisingh@users.noreply.github.com>
2019-04-19 21:43:24 -0400

    Updated links for recipe and post

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-<div><a class="mobilea" href="#"></a></div>
+<div><a class="mobilea" style="color:white;">Administration</a></div>
 <div id="contain">
   <div class="sidebar">
     <ul id="nav">
-      <li><a  href="admin_dashboard">Dashboard</a></li>
-      <li><a  href="user">Users</a></li>
-      <li><a href="recipe">Recipes</a></li>
+      <li><a  href="admin_dashboard">Search</a></li>
+      <li><a  href="useradmin">Users</a></li>
+      <li><a href="recipeadmin">Recipes</a></li>
       <li><a href="postadmin">Posts</a></li>
     </ul>
   </div>
